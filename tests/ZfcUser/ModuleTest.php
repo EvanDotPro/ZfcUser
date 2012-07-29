59261952d83673b0e6921e1d010815709fe4b750
Evan Coury <me@evancoury.com>
2012-07-29 03:30:33 -0700

    Initial phpunit bootstrap config

---
+++ tests/ZfcUser/ModuleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ZfcUserTest;
+
+use PHPUnit_Framework_TestCase;
+
+class ModuleTest extends PHPUnit_Framework_TestCase
+{
+    public function testIsTrueTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
