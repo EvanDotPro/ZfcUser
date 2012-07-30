2de1f9e875ed2a4e44f94a71834e438a3ea19c73
Evan Coury <me@evancoury.com>
2012-07-30 05:19:47 -0700

    s/TestConfiguration.php/TestConfig.php/

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -17,7 +17,7 @@ class Bootstrap
     public static function init()
     {
         // Load the user-defined test configuration file, if it exists; otherwise, load
-        if (is_readable(__DIR__ . '/TestConfiguration.php')) {
+        if (is_readable(__DIR__ . '/TestConfig.php')) {
             $testConfig = include __DIR__ . '/TestConfig.php';
         } else {
             $testConfig = include __DIR__ . '/TestConfig.php.dist';
